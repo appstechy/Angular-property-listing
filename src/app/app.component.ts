8b444ea3d47af4754cf75a50465581fbddbdafeb
Rinku <rinku@appstechy.com>
2023-08-18 15:04:04 +0530

    few changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,29 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   ],
   template: `
     <main>
-      <a [routerLink]="['/']">
+      <!-- <a [routerLink]="['/']">
         <header class="brand-name">
           <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+          
         </header>
-      </a>
+      </a> -->
+      <div style="padding: 10px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+">
+  <a [routerLink]="['/']" style="text-decoration:none;color:black">
+  <h2 style="text-align: center;">Property Listing App</h2>
+  </a>
+</div>
       <section class="content">
         <router-outlet></router-outlet>
       </section>
     </main>
+
+    <div style="padding: 10px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+">
+  
+  <p style="text-align: center;">Property Listing App</p>
+
+</div>
   `,
   styleUrls: ['./app.component.css'],
 })
